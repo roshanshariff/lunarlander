399ac9f9defbd9a082f33efd9985bb0d2f522de3
Roshan Shariff <roshan.shariff@ualberta.ca>
2012-04-16 01:08:28 -0600

    First draft of my tile coder

--- TileCoder.py
+++ TileCoder.py
@@ -57,6 +57,79 @@ class TileCoder:
             ix_offset += tiling.num_tiles
         return ixs
     
+class MultiTileCoder:
+
+    def __init__ (self, tile_size, num_tiles, num_offsets, subspace_dims):
+
+        self.tile_size = np.array (tile_size, dtype=np.float64)
+        self.num_tiles = np.array (num_tiles, dtype=np.intp)
+        self.num_offsets = np.array (num_offsets, dtype=np.intp)
+
+        space_dim = len(tile_size)
+
+        def select_subspace (ixs):
+            subspace = np.zeros (space_dim, dtype=np.bool)
+            subspace.put (ixs, True)
+            return subspace
+
+        self.subspaces = np.array(
+            map (select_subspace,
+                 itertools.chain.from_iterable (
+                    [ itertools.combinations (range(0, space_dim), dim)
+                      for dim in subspace_dims ])),
+            dtype=np.bool)
+
+        self.ixs_start = np.array(
+            list (itertools.chain.from_iterable (
+                    [ itertools.repeat (self.num_tiles.take(subspace).prod(),
+                                        self.num_offsets.take(subspace).prod())
+                      for subspace in subspaces ])),
+            dtype=np.intp).cumsum()
+
+        self.num_features = ixs_start[-1]
+        self.active_features = self.ixs_start.size
+
+    def indices (self, coord):
+
+        num_tiles = self.num_tiles
+        num_offsets = self.num_offsets
+
+        offset_start = coord / self.tile_size
+        tile_coord = np.floor (offset_coord)
+
+        offset_start -= tile_coord
+        offset_start *= offsets
+        offset_start = offset_start.astype(np.intp)
+        offset_start += 1
+
+        tile_coord = tile_coord.astype(np.intp)
+        tile_coord %= tiles
+
+        tile_coord_offset = tile_coord - 1
+        tile_coord_offset %= tiles
+
+        ixs = np.zeros (self.active_features, dtype=np.intp)
+        ixs_view = ixs.view()
+
+        for subspace in subspaces:
+
+            subspace_offsets = offsets[subspace]
+            num_subspace_offsets = subspace_offsets.prod()
+
+            ixs_offsets = ixs_view[:num_subspace_offsets]
+            ixs_offsets.shape = subspace_offsets
+
+            ixs_view = ixs_view[num_subspace_offsets:]
+
+            for i in subspace.nonzero():
+                
+                ixs_offsets *= tiles[i]
+                ixs_offsets[:offset_start[i]] += tile_coord[i]
+                ixs_offsets[offset_start[i]:] += tile_coord_offset[i]
+
+                ixs_offsets = np.rollaxis (ixs_offsets, 0, ixs_offsets.ndim)
+
+        ixs[1:] += self.ixs_start[:-1]
 
 class HashingTileCoder:
 
