089de7883e1e73c1c989b8b1405b5a56ef166f6d
Travis Dick <travis.barry.dick@gmail.com>
2012-04-22 17:23:15 -0600

    Experiment with no subspaces

--- Experiment.py
+++ Experiment.py
@@ -9,7 +9,7 @@ from PolicyGradientAgent import PolicyGradientAgent
 
 simulator = LunarLanderSimulator()
 
-def run_experiment(Lambda, alpha, tile_weight_exponent, num_runs, num_episodes=20000, num_procs=None):
+def run_experiment(Lambda, alpha, tile_weight_exponent, num_runs,num_episodes=20000, num_procs=None, suffixed=""):
     returns = np.empty((num_runs, num_episodes), dtype=np.float64)
     results.append(returns)
     for i in xrange(num_runs):
@@ -22,7 +22,7 @@ def run_experiment(Lambda, alpha, tile_weight_exponent, num_runs, num_episodes=2
         framework.train(num_procs=num_procs)
         returns[i] = framework.returns
     random = np.random.randint(sys.maxsize)
-    filename = 'data/experiment_%g_%g_%g_%d.npy'%(Lambda,alpha,tile_weight_exponent, random)
+    filename = 'data/experiment_%g_%g_%g_%d_%s.npy'%(Lambda,alpha,tile_weight_exponent, random, suffix)
     np.save (filename, returns)
     return returns
 
@@ -33,7 +33,7 @@ def make_plot():
         
 
 if __name__ == "__main__":
-    params = [ (0.75, 0.1, 0.5, 1) ]
+    params = [ (0.75, 0.1, 0.5, 1, 20000, None, "nosubspaces") ]
     results = []
     for ps in params:
         run_experiment(*ps)
