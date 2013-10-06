6d41460d4a842804a6b834be01ca7a741a521369
Roshan Shariff <roshan.shariff@ualberta.ca>
2013-10-06 17:34:48 -0600

    Import pyplot

--- PlotTools.py
+++ PlotTools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+import matplotlib.pyplot as plt
 
 def aggregate_results(results):
     results_sum = results.cumsum(axis=1)
@@ -9,10 +10,10 @@ def aggregate_results(results):
 
 def plot_results(results, label,  above=False):
     (mean, stderr) = aggregate_results(results)
-    fill_between(np.arange(results.shape[1]), mean+2*stderr, mean-2*stderr,
-                 color='moccasin', edgecolor='moccasin')
-    plot(mean, color='black', lw=2)
-    
+    plt.fill_between(np.arange(results.shape[1]), mean+2*stderr, mean-2*stderr,
+                     color='moccasin', edgecolor='moccasin')
+    plt.plot(mean, color='black', lw=2)
+
     xposition = results.shape[1] - 20
     if above:
         yposition = mean[xposition] + 2.5*stderr[xposition]
@@ -20,15 +21,13 @@ def plot_results(results, label,  above=False):
     else:
         yposition = mean[xposition] - 2.5*stderr[xposition]
         va = 'top'
-    text(xposition, yposition, label, va=va, ha='right', size=10, family='serif')
-    
+    plt.text(xposition, yposition, label, va=va, ha='right', size=10, family='serif')
+
 def set_labels(xtext, ytext):
-    xlabel(xtext, size=10, family='serif', ha='right', x=1)
-    ylabel(ytext, size=10, rotation='horizontal', multialignment='right',
-           ha='right', va='top', y=1.0, family='serif')
+    plt.xlabel(xtext, size=10, family='serif', ha='right', x=1)
+    plt.ylabel(ytext, size=10, rotation='horizontal', multialignment='right',
+               ha='right', va='top', y=1.0, family='serif')
 
 def set_tick_style():
-    xticks(size=9, family="serif")
-    yticks(size=9, family="serif")
-
-
+    plt.xticks(size=9, family="serif")
+    plt.yticks(size=9, family="serif")
