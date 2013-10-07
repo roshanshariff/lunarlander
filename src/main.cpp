2f1f5000b32b0a063681c6024c559dc701d1c001
Roshan Shariff <roshan.shariff@ualberta.ca>
2013-10-06 19:51:22 -0600

    Added command line options

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,6 @@
 int main (int argc, char* argv[]) {
 
   unsigned int seed = boost::random::random_device()();
-  if (argc > 1) seed = std::atoi (argv[1]);
-  //std::cout << "# Using seed: " << seed << std::endl;
 
   boost::random::mt19937 agent_rng(seed);
   boost::random::mt19937 init_rng(0);
@@ -23,6 +21,7 @@ int main (int argc, char* argv[]) {
   const double dt = 0.1;
   const int agent_time_steps = 5;
 
+  int num_episodes = 20000;
   double lambda = 0.75;
   double alpha_v = 0.1;
   double alpha_u = 0.1;
@@ -32,7 +31,8 @@ int main (int argc, char* argv[]) {
   bool trunc_normal = true;
 
   for (int i = 1; i < argc; ++i) {
-    if (std::string(argv[i]) == "--lambda") lambda = std::atof(argv[++i]);
+    if (std::string(argv[i]) == "--episodes") num_episodes = std::atoi(argv[++i]);
+    else if (std::string(argv[i]) == "--lambda") lambda = std::atof(argv[++i]);
     else if (std::string(argv[i]) == "--alpha-v") alpha_v = std::atof(argv[++i]);
     else if (std::string(argv[i]) == "--alpha-u") alpha_u = std::atof(argv[++i]);
     else if (std::string(argv[i]) == "--initial-value") initial_value = std::atof(argv[++i]);
@@ -56,10 +56,10 @@ int main (int argc, char* argv[]) {
               dt,
               agent_time_steps);
 
-  for (int i = 0; i < 20000; i++) {
-    f.set_visualiser (i % 1000 == 0 ? stdout : 0);
+  for (int i = 0; i < num_episodes; i++) {
+    //f.set_visualiser (i % 1000 == 0 ? stdout : 0);
     std::vector<double> rewards = f.run_episode(init_rng, agent_rng);
-    //std::fprintf(stdout, "%g\n", f.get_return());
+    std::fprintf(stdout, "%g\n", f.get_return());
     // if (f.simulator.get_landed()) std::printf("1 ");
     // else std::printf("0 ");
     // std::printf("%g\n", f.time_elapsed);
