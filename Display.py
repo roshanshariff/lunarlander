ababa2e1d59006c2211df86ff11d361c85a94a1c
Roshan Shariff <roshan.shariff@ualberta.ca>
2013-10-13 02:47:22 -0600

    Don't depend on lepton for particle rendering

--- Display.py
+++ Display.py
@@ -6,19 +6,10 @@ import pyglet.gl as gl
 from pyglet.window import key
 from pyglet.clock import ClockDisplay
 import numpy as np
+import ctypes
 
-try:
-    from lepton import Particle, ParticleGroup
-    from lepton.domain import Plane, Cylinder, Sphere
-    from lepton.controller import Lifetime, Movement, Bounce, Collector
-    from lepton.renderer import BillboardRenderer
-    from lepton.emitter import StaticEmitter
-    from lepton.texturizer import SpriteTexturizer
-    lepton_loaded = True
-except ImportError:
-    lepton_loaded = False
-    print('Lepton not found; particle effects disabled.')
-
+def numpy_to_ctype_array (array, dtype):
+    return array.ctypes.data_as(ctypes.POINTER(dtype*array.size)).contents
 
 class LunarLanderWindow (pyglet.window.Window):
 
@@ -27,10 +18,10 @@ class LunarLanderWindow (pyglet.window.Window):
         super(LunarLanderWindow, self).__init__(resizable=True, visible=False)
 
         self.framework = framework
-
         self.push_handlers (framework.agent)
-
         self.simulator = framework.simulator
+        self.dt = self.simulator.dt
+
         self.fov_diag = fov_diag
         self.scaleFactor = 100
 
@@ -79,29 +70,32 @@ class LunarLanderWindow (pyglet.window.Window):
                                          color=(255,255,255,255), anchor_x='center', anchor_y='top')
         self.landed.visible = False
 
-        if lepton_loaded:
-            self.puff_texture = pyglet.resource.texture('resources/puff.png')
-            ground_domain = Plane ([0,0,0], [0,1,0], half_space=True)
-            self.thruster = ParticleGroup (
-                controllers=[Lifetime(0.3), Movement(), Bounce(ground_domain, 0.01, -5), Collector(ground_domain)],
-                renderer=BillboardRenderer(SpriteTexturizer(self.puff_texture.id)))
-            self.rcs = ParticleGroup (
-                controllers=[Lifetime(0.1), Movement(), Collector(ground_domain)],
-                renderer=BillboardRenderer(SpriteTexturizer(self.puff_texture.id)))
+        self.puff_texture = pyglet.resource.texture('resources/puff.png')
+        self.particles = Thruster.make_domain()
+        self.thruster = Thruster (self.particles, self.scaleFactor, self.dt,
+                                  np.append(self.simulator.thruster_pos, 0), self.simulator.thruster_radius,
+                                  np.array([[1,0,0],[0,0,-1],[0,1,0]]).T, 30, self.simulator.thruster_spread,
+                                  2000, 0.5, 0.1)
+        self.rcs_left = Thruster (self.particles, self.scaleFactor, self.dt,
+                                  np.append(self.simulator.rcs_pos_left, 0), self.simulator.rcs_radius,
+                                  np.array([[0,1,0],[0,0,1],[1,0,0]]).T, 30, self.simulator.rcs_spread,
+                                  1000, 0.3, 0.25)
+        self.rcs_right = Thruster (self.particles, self.scaleFactor, self.dt,
+                                   np.append(self.simulator.rcs_pos_right, 0), self.simulator.rcs_radius,
+                                   np.array([[0,0,1],[0,1,0],[-1,0,0]]).T, 30, self.simulator.rcs_spread,
+                                   1000, 0.3, 0.25)
 
     def start (self, wait=0.0):
-        dt = self.simulator.dt
         pyglet.clock.unschedule (self.update)
-        pyglet.clock.schedule_once (lambda _: pyglet.clock.schedule_interval (self.update, dt), wait)
+        pyglet.clock.schedule_once (lambda _: pyglet.clock.schedule_interval (self.update, self.dt), wait)
 
     def update (self, _):
 
-        dt = self.simulator.dt
-        if not self.framework.run(dt, learn=False):
+        if not self.framework.run(self.dt, learn=False):
             print ('Return = {}'.format(self.framework.Return))
             self.start(1.0)
 
-        self.update_particles(dt)
+        self.update_particles()
 
         (self.lander.x, self.lander.y) = self.scaleFactor*self.simulator.lander.pos
         self.lander.rotation = -math.degrees(self.simulator.lander.rot)
@@ -111,49 +105,24 @@ class LunarLanderWindow (pyglet.window.Window):
         self.crashed.visible = self.simulator.crashed
         self.landed.visible = self.simulator.landed
 
-    def update_particles (self, dt):
+    def update_particles (self):
 
-        if not lepton_loaded: return
-
-        self.thruster.update(dt)
-        self.rcs.update(dt)
-
+        pos = np.append (self.simulator.lander.pos, 0)
+        vel = np.append (self.simulator.lander.vel, 0)
         cos_rot = math.cos(self.simulator.lander.rot)
         sin_rot = math.sin(self.simulator.lander.rot)
-        rot_matrix = np.array([[cos_rot, -sin_rot], [sin_rot, cos_rot]])
-
-        def exhaust_particles (group, opacity, vel, pos, radius, spread):
-
-            if opacity == 0: return
-
-            pos = self.simulator.lander.pos + np.dot(rot_matrix, pos)
-            vel = self.simulator.lander.vel + np.dot(rot_matrix, vel)
-            spread *= self.scaleFactor * np.linalg.norm(vel)
-
-            pos_domain = Cylinder(list(self.scaleFactor*pos)+[0.0],
-                                  list(self.scaleFactor*(pos-vel*dt))+[0.0],
-                                  self.scaleFactor*radius)
-            vel_domain = Sphere(list(self.scaleFactor*vel)+[0.0], spread)
-
-            size = self.scaleFactor * 2.0 * radius
-            emitter = StaticEmitter (template=Particle(color=(1,1,1,0.1*opacity), size=(size,size), age=-dt),
-                                     position=pos_domain, velocity=vel_domain)
-            emitter.emit(int(20*dt), group)
+        rot = np.array([[cos_rot, -sin_rot, 0], [sin_rot, cos_rot, 0], [0, 0, 1]])
+        rot_vel = np.array ([0, 0, self.simulator.lander.rot_vel])
 
         thrust = self.simulator.main_throttle()
-        exhaust_particles (self.thruster, thrust, [0,-50], self.simulator.thruster_pos,
-                           self.simulator.thruster_radius, self.simulator.thruster_spread)
-
         rcs = self.simulator.rcs_throttle()
-        if rcs > 0:
-            exhaust_particles (self.rcs, rcs, [50,0], self.simulator.rcs_pos_right,
-                               self.simulator.rcs_radius, self.simulator.rcs_spread)
-        else:
-            exhaust_particles (self.rcs, -rcs, [-50,0], self.simulator.rcs_pos_left,
-                               self.simulator.rcs_radius, self.simulator.rcs_spread)
 
+        self.thruster.update (thrust, pos, vel, rot, rot_vel)
+        self.rcs_left.update (max(0, -rcs), pos, vel, rot, rot_vel)
+        self.rcs_right.update (max(0, rcs), pos, vel, rot, rot_vel)
 
     def on_draw (self):
+
         self.clear()
 
         self.push_camera_projection()
@@ -166,13 +135,16 @@ class LunarLanderWindow (pyglet.window.Window):
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glPopMatrix()
 
-        if lepton_loaded:
-            gl.glEnable(gl.GL_BLEND)
-            gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE)
-            self.thruster.draw()
-            self.rcs.draw()
+        gl.glEnable(gl.GL_BLEND)
+        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE)
+        gl.glEnable (self.puff_texture.target)
+        gl.glBindTexture (self.puff_texture.target, self.puff_texture.id)
+
+        Thruster.draw(self.particles)
+        gl.glDisable (self.puff_texture.target)
 
         self.lander.draw()
+        gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glPopMatrix()
 
         gl.glMatrixMode(gl.GL_PROJECTION)
@@ -261,6 +233,111 @@ class LunarLanderWindow (pyglet.window.Window):
         self.crashed.y=self.height
         self.landed.x=self.width/2.0
         self.landed.y=self.height
+
+class Thruster (object):
+
+    tri_verts = (0, 1, 2, 2, 1, 3)
+
+    @staticmethod
+    def make_domain ():
+        return pyglet.graphics.vertexdomain.create_domain('v3f/stream', 't2s/static', 'c4f/stream')
+
+    @staticmethod
+    def draw (domain):
+        domain.draw(gl.GL_TRIANGLES)
+
+    def __init__ (self, domain, scale, dt, pos, radius, rot, vel, vel_spread, rate, duration, opacity):
+
+        self.scale = scale
+        self.dt = dt
+
+        self.thruster_pos = pos * scale
+        self.thruster_radius = radius * scale
+        self.thruster_rot = rot
+
+        self.exhaust_speed = vel*scale*dt
+        self.exhaust_vel = np.dot (rot, [0, 0, -self.exhaust_speed])
+        self.exhaust_vel_stddev = vel_spread*self.exhaust_speed
+
+        self.nparticles = int(math.ceil(dt * rate))
+        self.nframes = int(math.ceil(duration / dt))
+
+        self.positions = np.zeros((self.nframes, self.nparticles, 3), dtype=np.float32)
+        self.velocities = np.zeros((self.nframes, self.nparticles, 3), dtype=np.float32)
+        self.ixframe = 0
+
+        self.vertices = domain.create(self.nframes*self.nparticles*6)
+
+        self.init_tex_coords()
+        self.init_vert_colors()
+        self.init_vert_coords(2*self.thruster_radius)
+
+        self.particle_opacity = opacity
+
+    def init_tex_coords (self):
+        coords = [(x, y) for y in (1, 0) for x in (0, 1)]
+        tri_tex_coords = np.array([coords[i] for i in self.__class__.tri_verts], dtype=np.int16)
+        tex_coords = np.tile (tri_tex_coords, (self.nframes, self.nparticles, 1, 1))
+        self.vertices.tex_coords = numpy_to_ctype_array(tex_coords, gl.GLshort)
+
+    def init_vert_colors (self):
+        self.frame_colors = np.ones((self.nparticles, 6, 4), dtype=np.float32)
+        self.vertices.colors = numpy_to_ctype_array(np.tile (self.frame_colors, (self.nframes, 1)), gl.GLfloat)
+
+    def set_frame_opacity (self, frame, opacity):
+        n = self.frame_colors.size
+        self.frame_colors[...,3] = opacity * self.particle_opacity
+        self.vertices.colors[frame*n:(frame+1)*n] = numpy_to_ctype_array(self.frame_colors, gl.GLfloat)
+
+    def init_vert_coords (self, size):
+        coords = [(x/2.0, y/2.0, 0.0) for y in (-size, size) for x in (-size, size)]
+        self.tri_vert_coords = np.array([coords[i] for i in self.__class__.tri_verts], dtype=np.float32)
+        self.vert_coords = np.zeros((self.nframes, self.nparticles, 6, 3), dtype=np.float32)
+        self.update_vert_coords()
+
+    def update_vert_coords (self):
+        np.add (self.positions[...,np.newaxis,:], self.tri_vert_coords, out=self.vert_coords)
+        self.vertices.vertices = numpy_to_ctype_array(self.vert_coords, gl.GLfloat)
+
+    def move_particles (self):
+        self.positions += self.velocities
+        collided = self.positions[...,1] < 0
+        self.positions[collided,1] = 0
+        self.velocities[collided,0] *= 4.0
+        self.velocities[collided,1] *= -0.05
+        self.velocities[collided,2] *= 4.0
+
+    def update (self, thrust, pos, vel, rot, rot_vel):
+
+        self.move_particles()
+
+        self.set_frame_opacity(self.ixframe, thrust)
+        particle_pos = self.positions[self.ixframe]
+        particle_vel = self.velocities[self.ixframe]
+        self.ixframe = (self.ixframe + 1) % self.nframes
+
+        pos = pos * self.scale
+        vel = vel * self.scale * self.dt
+        rot_vel = rot_vel * self.dt
+        thruster_pos = np.dot(rot, self.thruster_pos)
+
+        buffer0 = np.random.normal (scale=self.exhaust_vel_stddev, size=(self.nparticles, 3))
+        buffer0 += vel + np.dot(rot, self.exhaust_vel) + np.cross(rot_vel, thruster_pos)
+        particle_vel[:] = buffer0
+
+        buffer1 = np.random.random_sample ((self.nparticles, 3))
+        buffer1 *= [2*math.pi, self.thruster_radius**2, self.exhaust_speed]
+        np.cos (buffer1[:,0], out=buffer0[:,0])
+        np.sin (buffer1[:,0], out=buffer0[:,1])
+        np.sqrt (buffer1[:,1], out=buffer1[:,1])
+        buffer0[:,:2] *= buffer1[:,1,np.newaxis]
+        buffer0[:,2] = buffer1[:,2]
+
+        np.dot (buffer0, np.dot(rot, self.thruster_rot).T, out=buffer1)
+        buffer1 += pos + thruster_pos
+        particle_pos[:] = buffer1
+
+        self.update_vert_coords()
 
 class UserAgent (pyglet.window.key.KeyStateHandler):
 
